812c1aaf7171cc3a0e6f2ee8bf08fc567160896d
raboy1997 <maks.rublya1997>
2018-05-20 20:11:24 +0300

    [Feature] add users feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,8 @@ class UsersController < ApplicationController
     end
   end
 
+  def profiles
+    @user = User.find_by(id: params[:id])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,25 @@ class User < ApplicationRecord
   has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
   has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
   has_many :personal_messages, dependent: :destroy
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
 
-
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
   def name
     email.split('@')[0]
   end
 
   def online?
     !Redis.new.get("user_#{self.id}_online").nil?
+  end
+
+  def follow(user_id)
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
   end
 
 end
